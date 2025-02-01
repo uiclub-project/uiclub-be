617d2a363437fe54bf3173a6ea82010d30b126fe
Nicolás Sierra <nico.sierra13@gmail.com>
2025-01-31 23:23:46 -0500

    add important comment

--- design_libraries/views/component_api_view.py
+++ design_libraries/views/component_api_view.py
@@ -4,7 +4,7 @@ from design_libraries.models.component import Component
 
 class ComponentDesignSystemsAPIView(generics.ListAPIView):
     serializer_class = ComponentSerializer
-
+#TODO: review why is design_systems and check what is reated name , check how this filter works?
     def get_queryset(self):
         design_system_id = self.kwargs.get("design_id")
         return Component.objects.filter(design_systems__pk=design_system_id)

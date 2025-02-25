2647c18df7fc4a1d7ff64610da430753a0bc3d87
Nicolás Sierra <nico.sierra13@gmail.com>
2025-02-24 22:25:38 -0500

    Add drf-spectacular

--- design_libraries/urls.py
+++ design_libraries/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('design-systems/', views.DesignSystemListAPIView.as_view(), name='design-system-list'),
     path('design-systems/<slug:slug>/', views.DesignSystemDetailAPIView.as_view(), name='design-system-list'),
-    path("design-systems/<int:design_id>/components/", views.ComponentDesignSystemsAPIView.as_view(), name="order-products"),
+    path("design-systems/<int:design_id>/components/", views.ComponentDesignSystemsAPIView.as_view(), name="design-components"),
+    path("design-systems/component/<int:component_id>/", views.ComponentDetailAPIView.as_view(), name="component"),
 ]

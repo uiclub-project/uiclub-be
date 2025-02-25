621c13393032a6930f7a30c8f8df04c4b45c7759
Nicolás Sierra <nico.sierra13@gmail.com>
2025-02-24 22:26:27 -0500

    Add settings of spectacular

--- uiclub_be/urls.py
+++ uiclub_be/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/news/", include("news.urls")),
     path("api/design-libraries/", include("design_libraries.urls")),
-    path('silk/', include('silk.urls', namespace='silk'))
+    path('silk/', include('silk.urls', namespace='silk')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 # Esto solo debe estar en desarrollo (no en producción)
